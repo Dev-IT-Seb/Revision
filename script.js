b44d8ed089a7c9cbcc0f81e84a32b4f66cf5f8a0
sebastien c. <schatel.pro@pm.me>
2025-03-29 11:11:04 +0100

    (feat) add carousel

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-// ------------------------------------------------------------------------ //
+//------------------------------------------------------------------------//
                             // HOME PAGE //
-// ------------------------------------------------------------------------ //
+//------------------------------------------------------------------------//
+//
+//------------------------------CAROUSEL----------------------------------//
+// BOUTONS
+let btnleft = document.querySelector(".cta-left");
+let btnright = document.querySelector(".cta-right");
+//SELECTION DE TOUTES LES IMAGES
+let images = document.querySelectorAll(".carousel img");
+let index = 0;
+//-----------------------------------------------//
+// BOUCLE POUR METTRE TOUTES LES IMAGES
+for (let i = 0; i < images.length; i++) {
+  images[i].style.display = "none";
+}
+//-----------------------------------------------//
+// AFFICHAGE DE LA 1ER IMAGE
+images[index].style.display = "block";
+//-----------------------------------------------//
+//BUTTON DROITE
+btnright.addEventListener("click", function() {
+
+  images[index].style.display = "none";
+  index = (index + 1) % images.length;
+  images[index].style.display = "block";
+});
+//-----------------------------------------------//
+//BUTTON GAUCHE
+btnleft.addEventListener("click", function() {
+  images[index].style.display = "none";
+  index = (index - 1 + images.length) % images.length;
+  images[index].style.display = "block";
+});
+//------------------------------------------------------------------------//
